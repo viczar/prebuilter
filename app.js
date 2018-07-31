e0b1c2871edf1e1b77a81531b2dbdf1178cd9529
Victor J. Salazar <vagol942@gmail.com>
2018-07-31 01:04:10 -0500

    Init

---
+++ app.js
@@ -0,0 +1,14 @@
+function sortList(list) {
+    
+}
+
+let app = new Vue({
+    el: "#content",
+    data: {
+        msg: "Garo",
+        prebuilts: []
+    },
+    created: function() {
+        fetch("prebuilds.json").then(res => res.json()).then(jason => this.prebuilts = jason);
+    }
+})
