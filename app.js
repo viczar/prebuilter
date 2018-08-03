8a17c7783f9b1e3a903c708ea8dfc32b9ea33fa6
Victor J. Salazar <vagol942@gmail.com>
2018-08-03 01:05:36 -0500

    Adds price selector

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
+function isNumeric(n) {
+    return !isNaN(parseFloat(n))
+}
+
 let app = new Vue({
     el: "#content",
     data: {
         msg: "Garo",
-        prebuilts: []
+        prebuilts: [],
+        minprice: "",
+        maxprice: "",
     },
     created: function() {
         fetch("prebuilds.json").then(res => res.json()).then(jason => {
             this.prebuilts = _.sortBy(jason, ['price'])
         });
+    },
+    computed: {
+        safeMinPrice: function() {
+            return isNumeric(this.minprice) ? parseFloat(this.minprice) : 500
+        },
+        safeMaxPrice: function() {
+            return isNumeric(this.maxprice) ? parseFloat(this.maxprice) : 2500
+        },
+        visiblePrebuilts: function() {
+            return this.prebuilts.filter(pre => pre.price >= this.safeMinPrice && pre.price <= this.safeMaxPrice)
+        }
     }
 })
